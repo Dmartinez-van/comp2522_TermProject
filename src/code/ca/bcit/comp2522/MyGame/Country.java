8c08da063983f432b4919187f093a335ba85abc7
David Martinez <Dmartinez21@my.bcit.ca>
2025-09-23 10:54:27 -0700

    Setup basic form for Country class

--- src/code/ca/bcit/comp2522/MyGame/Country.java
+++ src/code/ca/bcit/comp2522/MyGame/Country.java
@@ -2,4 +2,79 @@ package ca.bcit.comp2522.MyGame;
 
 public class Country
 {
+    private final String name;
+    private final String capitalCityName;
+
+    /*
+    0 -> fact 1,
+    1 -> fact 2, etc
+     */
+    private final int[] facts;
+
+    public Country(final String name,
+                   final String capitalCityName,
+                   final int[] facts)
+    {
+        checkName(name);
+        checkCapitalCityName(capitalCityName);
+        checkFacts(facts);
+
+        this.name = name;
+        this.capitalCityName = capitalCityName;
+        this.facts = facts;
+    }
+
+    private void checkName(final String name)
+    {
+        if (name == null || name.isBlank())
+        {
+            throw new IllegalArgumentException("Name cannot be null or blank");
+        }
+    }
+
+    private void checkCapitalCityName(final String capitalCityName)
+    {
+        if (capitalCityName == null || capitalCityName.isBlank())
+        {
+            throw new IllegalArgumentException("Capital city name cannot be null or blank");
+        }
+    }
+
+    private void checkFacts(final int[] facts)
+    {
+        if (facts == null || facts.length == 0)
+        {
+            throw new IllegalArgumentException("Facts cannot be null or empty");
+        }
+    }
+
+    /**
+     * Get the name of the country.
+     *
+     * @return the name of the country.
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * Get the name of the capital city.
+     *
+     * @return the name of the capital city.
+     */
+    public String getCapitalCityName()
+    {
+        return capitalCityName;
+    }
+
+    /**
+     * Get the facts of the country.
+     *
+     * @return the facts of the country.
+     */
+    public int[] getFacts()
+    {
+        return facts;
+    }
 }
