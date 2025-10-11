e08e832871be25e28cbc9765312308460b9c818e
David Martinez <Dmartinez21@my.bcit.ca>
2025-10-11 14:37:42 -0700

    setup main

--- src/code/ca/bcit/comp2522/WordGame/Main.java
+++ src/code/ca/bcit/comp2522/WordGame/Main.java
@@ -1,10 +1,52 @@
 package ca.bcit.comp2522.WordGame;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Contains main game loop
+ *
+ * @author David Martinez
+ * @version 1.0
+ */
 public class Main
 {
+    /**
+     * Runner method for the game loop
+     * @param args captures user's input to either continue playing game or end program
+     */
     public static void main(final String[] args)
     {
-        // runs the game
+        final Scanner sc;
+        final List<Score> scores;
 
+        sc     = new Scanner(System.in);
+        scores = new ArrayList<>();
+
+        while (sc.hasNextLine())
+        {
+            // TODO: If user enters "no" any case -> exit game & return to main menu
+            if (sc.next().equalsIgnoreCase("no"))
+            {
+                break;
+            }
+
+            if (sc.next().equalsIgnoreCase("yes"))
+            {
+                //TODO: Run game loop again
+                // new score object, etc. At end of game, add score to running
+                // List of scores.
+                break;
+            }
+
+            //TODO: If anything other than "yes" or "no" throw clear error message
+            // and repeat the prompt for user
+            else
+            {
+                //TODO: Might replace with constants -> OPTION_ONE and OPTION_TWO
+                System.out.println("ERROR: Must enter 'yes' or 'no'");
+            }
+        }
     }
 }
